7ccd90fce18497d52c2480ee58d6d5972d516d00
Filip Wojciechowski <fwojciec@gmail.com>
2023-03-01 21:34:56 -0800

    debug flag

--- build.zig
+++ build.zig
@@ -14,6 +14,12 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("zlox", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    const options = b.addOptions();
+    const debug = b.option(bool, "debug", "") orelse false;
+    options.addOption(bool, "debug", debug);
+    exe.addOptions("options", options);
+
     exe.install();
 
     const run_cmd = exe.run();
@@ -24,9 +30,11 @@ pub fn build(b: *std.build.Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
+    exe_tests.addOptions("options", options);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,22 @@
 const std = @import("std");
 const bytecode = @import("bytecode.zig");
 const vm = @import("vm.zig");
+const options = @import("options");
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-
     var chunk = bytecode.Chunk.init(allocator);
     var constant = try chunk.addConstant(1.2);
     try chunk.writeOpCode(bytecode.OpCode.op_constant, 123);
     try chunk.write(constant, 123);
     try chunk.writeOpCode(bytecode.OpCode.op_return, 123);
-    try bytecode.disassemble(std.io.getStdOut().writer(), &chunk);
+
+    if (options.debug) {
+        try bytecode.disassemble(std.io.getStdOut().writer(), &chunk);
+    }
+
     _ = try vm.interpret(&chunk);
     chunk.deinit();
 }
